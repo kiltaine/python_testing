2609a686558356d06fe64d9a73be6bdbac332e9d
kiltaine <jakub.nebrensky@gmail.com>
2025-08-03 20:58:26 +0200

    Update web_scraping_tutorial.py

--- python_testing/web_scraping_tutorial.py
+++ python_testing/web_scraping_tutorial.py
@@ -13,6 +13,43 @@ headers = test_table.find_all('th')
 header_titles = []
 for header in headers:
     header_titles.append(header.text[:-1])  # Remove trailing newline character
+
 all_rows = test_table.find_all('tr')
 data = all_rows[1:]
-print(data)
+
+
+first_row = data[0]
+first_row_data = first_row.find_all('td')
+
+
+data_texts = []
+for data_text in first_row_data:
+    data_texts.append(data_text.text[:-1])
+
+table_rows = []
+for row in data:
+    table_row = []
+    row_data = row.find_all('td')
+    for data_point in row_data:
+        table_row.append(data_point.text[:-1])
+    table_rows.append(table_row)
+
+
+filename = 'test_table.csv'
+f = open(filename, 'w')
+header_string = ''
+for title in header_titles:
+    header_string += title + ','
+header_string = header_string[:-1]
+header_string+= '\n'
+
+
+f.write(header_string)
+
+
+for row in table_rows:
+    row_string = ''
+    for column in row:
+        row_string += column + ','
+    row_string += '\n'
+    f.write(row_string)
