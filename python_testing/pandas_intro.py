231893615dd8ffb10d0c4d44c0305d0dde5e07b0
kiltaine <jakub.nebrensky@gmail.com>
2025-08-04 14:09:51 +0200

    new pandas files

---
+++ python_testing/pandas_intro.py
@@ -0,0 +1,17 @@
+
+import pandas as pd
+import numpy as np
+
+df_data = {
+    'col1' : np.random.rand(5),
+    'col2' : np.random.rand(5),
+    'col3' : np.random.rand(5)
+}
+
+df = pd.DataFrame(df_data)
+
+#print(df[:2])
+#print(df['col1'])
+
+
+
