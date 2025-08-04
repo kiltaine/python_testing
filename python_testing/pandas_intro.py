c648ebb761ddcebb61f62d320f710a77b107f4c0
kiltaine <jakub.nebrensky@gmail.com>
2025-08-04 16:20:25 +0200

    pandas changes

--- python_testing/pandas_intro.py
+++ python_testing/pandas_intro.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 import numpy as np
+import os
 
 df_data = {
     'col1' : np.random.rand(5),
@@ -13,5 +14,9 @@ df = pd.DataFrame(df_data)
 #print(df[:2])
 #print(df['col1'])
 
+tracks = pd.read_excel('Course Files/Tracks.xls', sheet_name=0)
 
 
+flights = pd.read_csv('flights.csv',index_col = False)
+print(flights[flights['ORIGIN'] == 'RNO'])
+#print(pd.unique(flights['DAY_OF_WEEK'].sort_values(ascending=False)))
